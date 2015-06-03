a36adf42bdf40082b8d472811614d6506479a31d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-03 18:52:57 -0400

    Add redisReply

--- src/redis.rs
+++ src/redis.rs
@@ -1,6 +1,15 @@
-use libc::{c_char, c_int, c_void, size_t, timeval};
+use libc::{c_char, c_int, c_longlong, c_void, size_t, timeval};
 
 use read::redisReader;
+
+#[repr(C)]
+pub struct redisReply {
+    pub kind: c_int, // type
+    pub integer: c_longlong,
+    pub len: c_int,
+    pub string: *mut c_char, // str
+    // ...
+}
 
 #[repr(C)]
 pub struct redisContext {
@@ -10,15 +19,16 @@ pub struct redisContext {
 }
 
 extern "C" {
+    pub fn redisReaderCreate() -> *mut redisReader;
+    pub fn freeReplyObject(reply: *mut c_void);
     pub fn redisConnect(ip: *const c_char, port: c_int) -> *mut redisContext;
 
     pub fn redisConnectWithTimeout(ip: *const c_char, port: c_int, tv: timeval)
                                    -> *mut redisContext;
 
-    pub fn redisReaderCreate() -> *mut redisReader;
+    pub fn redisReconnect(c: *mut redisContext) -> c_int;
     pub fn redisFree(c: *mut redisContext);
     pub fn redisGetReply(c: *mut redisContext, reply: *mut *mut c_void) -> c_int;
-    pub fn freeReplyObject(reply: *mut c_void);
 
     pub fn redisAppendCommandArgv(c: *mut redisContext, argc: c_int, argv: *const *const c_char,
                                   argvlen: *const size_t);
